f5560d0abe2d64e1cfa1dac7d93d9b3ab1373cc6
ddwdwaw1111 <55290301+ddwdwaw1111@users.noreply.github.com>
2022-06-20 16:59:10 -0400

    take TaskServer out of main.go

---
+++ backend/db/taskserver/taskserver.go
@@ -0,0 +1,63 @@
+package taskserver
+
+import (
+	"time"
+
+	"github.com/internal/taskstore"
+	"github.com/kataras/iris/v12"
+)
+
+type TaskServer struct {
+	Store *taskstore.TaskStore
+}
+
+func NewTaskServer() *TaskServer {
+	store := taskstore.New()
+	return &TaskServer{Store: store}
+}
+
+//handler
+
+func (ts *TaskServer) GetAll(ctx iris.Context) {
+	ctx.JSON(ts.Store.GetAllTasks())
+}
+
+func (ts *TaskServer) CreateNew(ctx iris.Context) {
+	var task taskstore.Task
+	err := ctx.ReadJSON(&task)
+
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.WriteString(err.Error())
+		return
+	}
+
+	ts.Store.CreateTask(task.Text, time.Now())
+	ctx.JSON(ts.Store.GetAllTasks())
+}
+
+func (ts *TaskServer) DeleteTask(ctx iris.Context) {
+	var task taskstore.Task
+	err := ctx.ReadJSON(&task)
+
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.WriteString(err.Error())
+		return
+	}
+	ts.Store.DeleteTask(task.Id)
+	ctx.JSON(ts.Store.GetAllTasks())
+}
+
+func (ts *TaskServer) UpdateComplete(ctx iris.Context) {
+	var task taskstore.Task
+	err := ctx.ReadJSON(&task)
+
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.WriteString(err.Error())
+		return
+	}
+	ts.Store.UpdateTask(task)
+	ctx.JSON(ts.Store.GetAllTasks())
+}
--- backend/main.go
+++ backend/main.go
@@ -5,79 +5,19 @@ import (
 	"time"
 
 	"github.com/db/sqldb"
+	"github.com/db/taskserver"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/internal/taskstore"
 	"github.com/kataras/iris/v12"
 )
-
-type mypage struct {
-	Id int `json:"id"`
-}
-
-//server
-type taskServer struct {
-	store *taskstore.TaskStore
-}
-
-func NewTaskServer() *taskServer {
-	store := taskstore.New()
-	return &taskServer{store: store}
-}
-
-//handler
-
-func (ts *taskServer) getAll(ctx iris.Context) {
-	ctx.JSON(ts.store.GetAllTasks())
-}
-
-func (ts *taskServer) createNew(ctx iris.Context) {
-	var task taskstore.Task
-	err := ctx.ReadJSON(&task)
-
-	if err != nil {
-		ctx.StatusCode(iris.StatusBadRequest)
-		ctx.WriteString(err.Error())
-		return
-	}
-
-	ts.store.CreateTask(task.Text, time.Now())
-	ctx.JSON(ts.store.GetAllTasks())
-}
-
-func (ts *taskServer) deleteTask(ctx iris.Context) {
-	var task taskstore.Task
-	err := ctx.ReadJSON(&task)
-
-	if err != nil {
-		ctx.StatusCode(iris.StatusBadRequest)
-		ctx.WriteString(err.Error())
-		return
-	}
-	ts.store.DeleteTask(task.Id)
-	ctx.JSON(ts.store.GetAllTasks())
-}
-
-func (ts *taskServer) updateComplete(ctx iris.Context) {
-	var task taskstore.Task
-	err := ctx.ReadJSON(&task)
-
-	if err != nil {
-		ctx.StatusCode(iris.StatusBadRequest)
-		ctx.WriteString(err.Error())
-		return
-	}
-	ts.store.UpdateTask(task)
-	ctx.JSON(ts.store.GetAllTasks())
-}
 
 func main() {
 	db := sqldb.ConnectDB()
 	fmt.Print(db)
 	//Create a new store and add three test to it.
-	tasks := NewTaskServer(db)
-	tasks.store.CreateTask("test0", time.Now().Add(time.Hour*24))
-	tasks.store.CreateTask("test1", time.Now().Add(time.Hour*24))
-	tasks.store.CreateTask("test2", time.Now().Add(time.Hour*24))
+	tasks := taskserver.NewTaskServer()
+	tasks.Store.CreateTask("test0", time.Now().Add(time.Hour*24))
+	tasks.Store.CreateTask("test1", time.Now().Add(time.Hour*24))
+	tasks.Store.CreateTask("test2", time.Now().Add(time.Hour*24))
 	// fmt.Println(alltasks)
 
 	//Start iris serve
@@ -88,14 +28,13 @@ func main() {
 	{
 		tasksAPI.Use(iris.Compression)
 
-		tasksAPI.Get("/", tasks.getAll)
+		tasksAPI.Get("/", tasks.GetAll)
 
-		tasksAPI.Post("/", tasks.createNew)
+		tasksAPI.Post("/", tasks.CreateNew)
 
-		tasksAPI.Patch("/", tasks.updateComplete)
+		tasksAPI.Patch("/", tasks.UpdateComplete)
 
-		tasksAPI.Delete("/", tasks.deleteTask)
-		// UPDATE : http://localhost:8080/books/<taskId>
+		tasksAPI.Delete("/", tasks.DeleteTask)
 	}
 
 	app.Listen(":8080")
